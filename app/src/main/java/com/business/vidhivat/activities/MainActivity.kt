d467c05e7098aefe7ce044e21b9036a25eaa6049
Priyam <priyam tayade>
2021-07-11 19:45:45 +0530

    First Commit by priyam

---
+++ app/src/main/java/com/business/vidhivat/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.business.vidhivat.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.business.vidhivat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
